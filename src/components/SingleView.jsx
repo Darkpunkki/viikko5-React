f12e3e2dd05526d42b8f2d9c83ebd92480e3f891
Darkpunkki <jimi.pettila@metropolia.fi>
2024-04-15 13:40:28 +0300

    m

---
+++ src/components/SingleView.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+const SingleView = (props) => {
+  const {selectedItem, setSelectedItem} = props;
+  const handleClick = () => {
+    setSelectedItem(null);
+  };
+  return (
+    <>
+      <dialog open={selectedItem ? true : false}>
+        <p>
+          <button onClick={handleClick}>Close</button>
+        </p>
+        {selectedItem && (
+          <img src={selectedItem.filename} alt={selectedItem.title} />
+        )}
+      </dialog>
+    </>
+  );
+};
+
+SingleView.propTypes = {
+  selectedItem: PropTypes.object,
+  setSelectedItem: PropTypes.func.isRequired,
+};
+
+export default SingleView;
